81eaa4935e7869daddf10050d62bfa518d389730
yv <vasilchenko.yegor@gmail.com>
2016-11-23 17:31:54 +0200

    Can now show user details by ID

--- scrap_group/views.py
+++ scrap_group/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
+
+import vk
+
+from scrap_group.models import UserIds
 
 
 class GroupWall(TemplateView):
@@ -7,6 +13,39 @@ class GroupWall(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(GroupWall, self).get_context_data(**kwargs)
 
-        context['posts'] = []
+        session = vk.Session()
+        api = vk.API(session)
+        user = api.users.get(user_ids=1)
+
+        context['data'] = user
+
+        return context
+
+
+def user_id(request):
+    if request.method == 'GET':
+        return render(request, 'scrap_group/choose_user.html')
+
+    if request.method == 'POST':
+        uid = UserIds(user_id=request.POST['user_id'])
+        uid.save()
+        return redirect(reverse('user_details'))
+
+
+class UserDetails(TemplateView):
+    template_name = 'scrap_group/user_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetails, self).get_context_data(**kwargs)
+
+        session = vk.Session()
+        api = vk.API(session)
+
+        uid = UserIds.objects.last().user_id
+        print(uid)
+
+        user = api.users.get(user_ids=uid)
+
+        context['user_details'] = user
 
         return context
--- vk_test/urls.py
+++ vk_test/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from scrap_group.views import GroupWall
+from scrap_group.views import GroupWall, user_id, UserDetails
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', GroupWall.as_view(), name='wall'),
+    url(r'^user_id$', user_id, name='user_id'),
+    url(r'^user_details$', UserDetails.as_view(), name='user_details'),
 ]
