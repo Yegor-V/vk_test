b6436b7b6b4f6aeccde8cb99483487a9c3edd52f
yv <vasilchenko.yegor@gmail.com>
2016-11-28 17:00:57 +0200

    Show friends by id

--- scrap_group/views.py
+++ scrap_group/views.py
@@ -15,7 +15,7 @@ class GroupWall(TemplateView):
 
         session = vk.Session()
         api = vk.API(session)
-        user = api.users.get(user_ids=1)
+        user = api.users.get(user_ids=1, fields='sex,bdate,city')
 
         context['data'] = user
 
@@ -42,10 +42,27 @@ class UserDetails(TemplateView):
         api = vk.API(session)
 
         uid = UserIds.objects.last().user_id
-        print(uid)
 
         user = api.users.get(user_ids=uid)
 
+        friends_ids = api.friends.get(user_id=397905612)
+        friends = api.users.get(user_ids=friends_ids, fields='sex')
+        men_count = 0
+        women_count = 0
+        dafaq_count = 0
+        for friend in friends:
+            if friend['sex'] == 1:
+                women_count += 1
+            elif friend['sex'] == 2:
+                men_count += 1
+            else:
+                dafaq_count += 1
         context['user_details'] = user
+        context['friends'] = friends
+        context['men_count'] = men_count
+        context['women_count'] = women_count
+
+        # Wall last two posts (betsbyboss group)
+        wall_posts = api.wall.get(domain='betsbyboss', count=1)
 
         return context
